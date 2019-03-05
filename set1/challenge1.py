18d9ff781d22eb67455bfa34bb1e8af3df9095a7
Steve Tornio <swtornio@gmail.com>
2019-03-05 09:00:07 -0600

    challenge 1 works

---
+++ set1/challenge1.py
@@ -0,0 +1,23 @@
+# Convert hex to base64
+# The string:
+
+# 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# Should produce:
+
+# SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+import base64
+orig = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+expected_b64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+raw_bytes = bytes.fromhex(orig)
+
+encoded = base64.b64encode(raw_bytes).decode('ascii')
+
+print(encoded, "should be SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+if encoded == expected_b64:
+	print("it is!")
+else:
+	print("try again")
+
+
