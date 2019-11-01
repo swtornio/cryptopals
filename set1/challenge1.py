7b0aff41bc2a686925ab316f9c5d538f8dcad47a
swtornio <steve@vitriol.net>
2019-10-31 21:34:08 -0500

    one more change to verify git

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -5,9 +5,9 @@
 # Should produce:
 
 # SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+import base64
 
 def main():
-	import base64
 	orig = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 	expected_b64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
